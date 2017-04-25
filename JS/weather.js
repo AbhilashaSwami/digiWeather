3ee7826f6ce728339864f1ea72db98d109c0d737
AbhilashaSwami <abhilasha.swami94@gmail.com>
2017-04-25 12:25:08 +0530

    first commit

--- JS/weather.js
+++ JS/weather.js
@@ -20,7 +20,7 @@ $(function()
         $.ajax
         ({
             type:'GET',
-           url:'http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&APPID=8c7f8f1de1b06ab3eff2c6af2eefcdf4',
+            url:'http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&APPID=8c7f8f1de1b06ab3eff2c6af2eefcdf4',
             success: function(data)
             {    
                 if(data.Error)
@@ -31,13 +31,14 @@ $(function()
                else
                {
                 console.log(data);
-                var date=new Date(data.dt);
+                var date=new Date();
                 $output.append("<br> <br>Location: "+data.name+
                     "<br>Latitude: "+data.coord.lat+"<br>" + 
                     "Longitude: "+data.coord.lon+" <br>Date: "+date+" <h2>"+data.weather[0].main+"</h2>"
                     +data.weather[0].description+
                     "<br><br>Temperature: "+data.main.temp+
-                    "F<br>Min. Temp: " +"" +data.main.temp_min+" F<br>Max. Temp: "+data.main.temp_max+" F<br>Pressure: "+data.main.pressure+" ")
+                    "F<br>Min. Temp: " +"" +data.main.temp_min+
+                    " F<br>Max. Temp: "+data.main.temp_max+" F<br>Pressure: "+data.main.pressure+" ")
 
                }
 
